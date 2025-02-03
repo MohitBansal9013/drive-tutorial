8d3f5230e84bd338c1a98e0d0f946ebe6bbf8ce5
BANSAL Mohit <mohit.bansal@soprasteria.com>
2025-02-03 15:19:22 +0530

    Test PR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        {"Sup Nerds, you're are subscribed rite ? Hi There"}
+        {
+          "Sup Nerds, you're are subscribed rite ? Hi There, This is test change with a PR"
+        }
       </div>
     </main>
   );
